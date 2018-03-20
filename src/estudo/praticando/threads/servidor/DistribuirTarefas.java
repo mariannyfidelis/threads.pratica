a2d0b54e7c0afaf5d020a8cea1af10120d815c94
mariannyfidelis <mariannyfidelis@gmail.com>
2018-03-20 17:37:47 -0300

    Atualizando para implementar também a interface Callable

--- src/estudo/praticando/threads/servidor/DistribuirTarefas.java
+++ src/estudo/praticando/threads/servidor/DistribuirTarefas.java
@@ -5,6 +5,7 @@ import java.net.Socket;
 import java.util.Scanner;
 import java.util.concurrent.ExecutorService;
 
+
 public class DistribuirTarefas implements Runnable {
 
 	private Socket socket;
@@ -34,27 +35,46 @@ public class DistribuirTarefas implements Runnable {
 				System.out.println("Comando recebido " + comando);
 
 				switch (comando) {
-					case "c1": {
-						// confirmação do o cliente
-						saidaCliente.println("Confirmação do comando c1");
-						ComandoC1 c1 = new ComandoC1(saidaCliente);
-						this.threadPool.execute(c1);
-						break;
-					}
-					case "c2": {
-						saidaCliente.println("Confirmação do comando c2");
-						ComandoC2 c2 = new ComandoC2(saidaCliente);
-						this.threadPool.execute(c2);
-						break;
-					}
-					case "fim": {
-						saidaCliente.println("Desligando o servidor");
-						servidor.parar();
-						return;
-					}
-					default: {
-						saidaCliente.println("Comando não encontrado");
-					}
+				case "c1": {
+					// confirmação do o cliente
+					saidaCliente.println("Confirmação do comando c1");
+					ComandoC1 c1 = new ComandoC1(saidaCliente);
+					this.threadPool.execute(c1);
+					break;
+				}
+				case "c2": {
+					saidaCliente.println("Confirmação do comando c2");
+					ComandoC2 c2 = new ComandoC2(saidaCliente);
+					this.threadPool.execute(c2);
+
+					/*
+					 * Tarefas com retorno semelhante ao execute. Porém recebe um Callable
+					 * Diferentemente do execute() que recebe um Runnable.
+					 * 
+					 * O método submit não bloqueia a execução e podemos submeter quantas tarefas
+					 * quisermos e retorna um Future generics <T> nesse caso uma String.
+					 * 
+					 * 
+					 * Future<String> futureBD = this.threadPool.submit(new
+					 * ComandoC2AcessaBanco(saidaCliente)); String resultadoDoCallableBD =
+					 * futureBD.get();
+					 * 
+					 * Future<String> futureWS = this.threadPool.submit(new
+					 * ComandoC2WS(saidaCliente)); String resultadoDoCallableWS = futureWS.get();
+					 * 
+					 * this.threadPool.submit(new JuntaResultadosFutureWSFutureBanco(futureWS,
+					 * futureBD, saidaCliente));
+					 */
+					break;
+				}
+				case "fim": {
+					saidaCliente.println("Desligando o servidor");
+					servidor.parar();
+					return;
+				}
+				default: {
+					saidaCliente.println("Comando não encontrado");
+				}
 				}
 
 				System.out.println(comando);
